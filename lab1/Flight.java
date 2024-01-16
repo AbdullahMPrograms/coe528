4151c7a906af4c975ffdbffb97925bfa19e53510
AbdullahMPrograms <abdullahmalik1385@gmail.com>
2024-01-16 16:48:55 -0500

    lab1 complete (no junit testing, can do inclass)

---
+++ lab1/Flight.java
@@ -0,0 +1,100 @@
+package lab1;
+
+public class Flight {
+    // Instance Variables
+    private int flightNumber;
+    private String origin;
+    private String destination;
+    private String departureTime;
+    private int capacity;
+    private int numberOfSeatsLeft;
+    private double originalPrice;
+
+    // Constructor
+    public Flight(int flightNumber, String origin, String destination, String departureTime, int capacity, int numberOfSeatsLeft, double originalPrice) {
+        if(origin.equals(destination)) {
+            throw new IllegalArgumentException("Origin and Destination of flight cannot be the same.");
+        }
+        
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.capacity = capacity;
+        this.numberOfSeatsLeft = numberOfSeatsLeft;
+        this.originalPrice = originalPrice;
+    }
+
+    // Getters/Setters
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+     
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+     
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setNumberOfSeatsLeft(int numberOfSeatsLeft) {
+        this.numberOfSeatsLeft = numberOfSeatsLeft;
+    }
+     
+    public int getNumberOfSeatsLeft() {
+        return numberOfSeatsLeft;
+    }
+
+    public void setOriginalPrice(double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    //bootASeat(), boolean, condition is if numberOfSeatsLeft > 0
+    public boolean bookASeat() {
+        if(numberOfSeatsLeft > 0) {
+            numberOfSeatsLeft--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // toString() Override
+    @Override
+    public String toString() {
+        return "Flight " + flightNumber + ", " + origin + " to " + destination + ", " + departureTime + ", " + "original price: $" + originalPrice;
+    }
+}
--- lab1/Manager.java
+++ lab1/Manager.java
@@ -1,26 +1,31 @@
+package lab1;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Manager {
+    // Instance Variables
     private List<Flight> flights = new ArrayList<>();
     private List<Ticket> tickets = new ArrayList<>();
 
+    // Methods
     public void createFlights() {
-        flights.add(new Flight(1, "Toronto", "Vancouver", "10:00", 100, 500.0));
-        flights.add(new Flight(2, "Toronto", "Montreal", "12:00", 50, 300.0));
+        // TEST
+        flights.add(new Flight(1, "Toronto", "Vancouver", "10:00", 100, 5, 500.0));
+        flights.add(new Flight(2, "Toronto", "Montreal", "12:00", 50, 3, 300.0));
     }
 
     public void displayAvailableFlights(String origin, String destination) {
-        for (Flight flight : flights) {
-            if (flight.getOrigin().equals(origin) && flight.getDestination().equals(destination) && flight.getNumberOfSeatsLeft() > 0) {
+        for(Flight flight:flights) {
+            if(flight.getOrigin() == origin && flight.getDestination() == destination && flight.getNumberOfSeatsLeft() > 0) {
                 System.out.println(flight);
             }
         }
     }
 
     public Flight getFlight(int flightNumber) {
-        for (Flight flight : flights) {
-            if (flight.getFlightNumber() == flightNumber) {
+        for(Flight flight:flights) {
+            if(flight.getFlightNumber() == flightNumber) {
                 return flight;
             }
         }
@@ -29,7 +34,7 @@ public class Manager {
 
     public void bookSeat(int flightNumber, Passenger p) {
         Flight flight = getFlight(flightNumber);
-        if (flight != null && flight.bookASeat()) {
+        if(flight!= null && flight.bookASeat()) {
             double price = p.applyDiscount(flight.getOriginalPrice());
             Ticket ticket = new Ticket(p, flight, price);
             tickets.add(ticket);
@@ -39,11 +44,11 @@ public class Manager {
         }
     }
 
-    public static void main(String[] args) {
-        Manager manager = new Manager();
+    public static void main(String[] args ){
+                Manager manager = new Manager();
         manager.createFlights();
         manager.displayAvailableFlights("Toronto", "Vancouver");
-        Passenger passenger = new Member("John Doe", 30, 6);
+        Passenger passenger = new Member(30, "John Doe", 6);
         manager.bookSeat(1, passenger);
     }
 }
